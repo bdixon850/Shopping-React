8897010c9225c0679c34c46c848909bd0081e6b2
Brandon Dixon <bdixon850@yahoo.com>
2019-07-31 10:03:09 -0400

    cart components

---
+++ src/components/Cart/CartList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CartItem from './CartItem';
+
+export default function CartList() {
+    return (
+        <div>
+            <h1>hello from cart list</h1>
+            <CartItem />
+        </div>
+    )
+}
--- src/components/Product.js
+++ src/components/Product.js
@@ -25,9 +25,10 @@ export default class Product extends Component {
                                 <button 
                                     className="cart-btn" 
                                     disabled={inCart ? true : false} 
-                                    onClick = {() => 
-                                        value.addToCart(id)
-                                    }
+                                    onClick = {() => {
+                                        value.addToCart(id);
+                                        value.openModal(id);
+                                    }}
                                 >
                                     {inCart? (<p className="text-capitalize mb-0" disabled>in cart</p>):(<i className="fas fa-cart-plus" />)}
                                 </button>
